8587cfa9de1f03657ffd5e01cbed2bc37cfc73fe
Evg <velevic@gmail.com>
2025-05-05 11:54:45 +0300

    Hellogit add .!

--- main.cpp
+++ main.cpp
@@ -4,6 +4,6 @@
 
 int main(int, char **) {
 	std::cout << "Version: " << version() << std::endl;
-	std::cout << "Hello, world!" << std::endl;
+	std::cout << "Hello!!! This is my first HomeWork!" << std::endl;
 	return 0;
 }
