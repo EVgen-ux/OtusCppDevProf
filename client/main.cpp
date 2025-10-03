12f8332e496c05cf6d92a92b97dc4fc212d13334
Evg <velichko-ev@mail.ru>
2025-10-03 22:44:41 +0300

    ввод команд с консоли

--- client/main.cpp
+++ client/main.cpp
@@ -1,15 +1,13 @@
 #include <iostream>
-#include <fstream>
 #include <string>
 #include <thread>       
 #include <chrono>       
-
 
 #include <async_bulk/async.h>
 
 int main(int argc, char* argv[]) {
     if (argc < 2) {
-        std::cout << "The program accepts exaxtly one integral argument" << std::endl;
+        std::cout << "The program accepts exactly one integral argument" << std::endl;
         std::cout << "Example: ./bulk 3" << std::endl;
         return 1;
     }
@@ -24,23 +22,23 @@ int main(int argc, char* argv[]) {
 
     auto ctx = AsyncBulk::connect(blockSize);
 
-    std::ifstream commandsFile("commands.txt");
-    
-    if (!commandsFile.is_open()) {
-        std::cerr << "Error: Could not open commands.txt\n";
-        AsyncBulk::disconnect(ctx);
-        return 1;
-    }
-
+    std::cout << "Enter commands (type 'EOF' on new line to finish):" << std::endl;
     std::string command;
 
-    while (std::getline(commandsFile, command)) {
+    while (std::getline(std::cin, command)) {
+        if (command == "EOF") {
+            break;
+        }
         AsyncBulk::receive(ctx, command);
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
     }
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
 
     AsyncBulk::disconnect(ctx);
     
-    std::this_thread::sleep_for(std::chrono::seconds(1));
+    std::this_thread::sleep_for(std::chrono::seconds(100));
     AsyncBulk::shutdown();
 
     return 0;
